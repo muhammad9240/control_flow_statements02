37a7edb81f38bac78e4ee88ddf138b31d636fbef
muhammad9240 <muhammad92401111@gmail.com>
2023-04-28 11:04:56 +0500

    dndh

--- bin/if01.dart
+++ bin/if01.dart
@@ -7,6 +7,18 @@ Args:
     c: Third number.
 Returns:
     int: return answer.
-*/
+*/int func(int a, int b, int c,){
+if(a > b && a > c){
+    return(a);
+}
+if(b > a && b > c){
+    return(b);
+}
+if(c > a && c > b){
+    return(c);
+}
+return 0;
+}
 void main() {
+    print(func(13, 4, 7));
 }
