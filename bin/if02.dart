2de256ee4a91931d5b996e680331173a220bf108
muhammad9240 <muhammad92401111@gmail.com>
2023-04-28 14:04:16 +0500

    hfhf

--- bin/if02.dart
+++ bin/if02.dart
@@ -7,6 +7,18 @@ Args:
     c: Third number.
 Returns:
     int: return answer.
-*/
-void main() {
+*/int func(int a, int b, int c){
+    if(a < b && a < c){
+       return(a); 
+    }
+    if(b < a && b < c){
+        return(b);
+    }
+    if(c < a && c < b){
+        return(c);
+    }
+    return 0;
+}
+void main(){
+ print(func(3, 6, 10));   
 }
